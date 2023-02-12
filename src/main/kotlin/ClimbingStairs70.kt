cb29b298b84801cae94431090aedd34ca6f958bf
Rustam <antinov.rus@mail.ru>
2023-02-12 14:27:32 +0700

    70. Climbing Stairs

--- src/main/kotlin/ClimbingStairs70.kt
+++ src/main/kotlin/ClimbingStairs70.kt
@@ -1,4 +1,7 @@
-class Solution {
+
+//You are climbing a staircase. It takes n steps to reach the top.
+//Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+class ClimbingStairs70 {
     fun climbStairs(n: Int): Int {
         if(n <= 1) return 1
 
